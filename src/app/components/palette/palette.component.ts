e541e68d55f8035808c347e4a3aad69978924d28
eva-wamuyu <ruth.wamuyu@aws.ajiraemobilis.org>
2024-08-08 13:14:11 +0300

    .

--- src/app/components/palette/palette.component.ts
+++ src/app/components/palette/palette.component.ts
@@ -6,35 +6,17 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./palette.component.css']
 })
 export class PaletteComponent {
-  colors = [
-    '#000000',
-    '#FFFFFF',
-    '#808080',
-    '#FF0000',
-    '#00FF00',
-    '#0000FF',
-    '#FFFF00',
-    '#FFA500',
-    '#800080',
-    '#FFC0CB',
-    '#008080',
-    '#FFD700',
-    '#A52A2A',
-    '#FF1493',
-  ];
 
   selectedColor: string = '#000000';
   @Output() colorSelected = new EventEmitter<string>();
 
   @Output() backgroundColorSelected = new EventEmitter<string>();
 
-  selectColor(event?: Event,color?:string) {
-    if(color) this.colorSelected.emit(color);
+  selectColor(event: Event) {
     if (event){
     let color = (event?.target as HTMLInputElement).value;
     this.colorSelected.emit(color);
     }
-    this.selectedColor = color ?? '';
   }
 
   selectBackgroundColor(color: string) {
